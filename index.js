bb484ae45760d67ca0dd1875dff4755e762960e3
alisabaj <alisabaj@gmail.com>
2019-12-03 13:29:41 -0800

    fetch request for users and putting data on html

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
 const base_url = "http://localhost:3000"
 const projects_url = `${base_url}/projects`
+const users_url = `${base_url}/users`
 
 
 console.log("Here we go")
-fetch(projects_url)
+// fetch(projects_url)
+// .then(resp => resp.json())
+// .then(json => {
+//     json.forEach(project => {
+//         let data = formatProjectJsonData(project)
+//         createprojectCard(data) 
+//     })
+// })
+
+fetch(users_url)
 .then(resp => resp.json())
 .then(json => {
-    json.forEach(project => {
-        let data = formatProjectJsonData(project)
-        createprojectCard(data) 
+    json.forEach(user => {
+        let data = formatUserJsonData(user)
+        createUserCard(data)
     })
-
-    // json.forEach(user => {
-    //     let userCard = formatUserJsonData(user)
-    //     createuserCard(userCard)
-    // })
 })
+
+function formatUserJsonData(user) {
+    let hash = {};
+    hash["username"] = user.username;
+    hash["email"] = user.email;
+    hash["location"] = user.location;
+    return hash;
+}
+
+function createUserCard(hash) {
+    console.log(hash)
+    let div = document.querySelector('#userInfo')
+    let userDiv = document.createElement('div')
+    userDiv.classList.add('user_div')
+    div.appendChild(userDiv)
+
+    let pUsername = document.createElement('p')
+    pUsername.textContent = hash.username;
+    let pEmail = document.createElement('p')
+    pEmail.textContent = hash.email
+    let pLocation = document.createElement('p')
+    pLocation.textContent = hash.location
+    userDiv.appendChild(pUsername)
+    userDiv.appendChild(pEmail)
+    userDiv.appendChild(pLocation)
+
+    let editButton = document.createElement('button')
+    editButton.textContent = "Edit"
+    editButton.addEventListener('click', () => {
+        console.log("click")
+        pUsername.contentEditable = true;
+        pEmail.contentEditable = true;
+        pLocation.contentEditable = true;
+    })
+    userDiv.appendChild(editButton)
+}
 
 function formatProjectJsonData(project) {
     let newHash = {}
@@ -24,6 +65,9 @@ function formatProjectJsonData(project) {
     newHash["status"] = project.status
     return newHash
 }
+
+
+
 
 function createprojectCard(hash) {
     let h2 = document.querySelector('#projectID')
@@ -36,30 +80,6 @@ function createprojectCard(hash) {
     let status = document.createElement('p')
     status.textContent = hash.status
     div.appendChild(status)
-    let location = document.createElement('p')
-    location.textContent = hash.location
-    div.appendChild(location)
-}
-
-function formatUserJsonData(user) {
-    let newHash = {}
-    newHash["username"] = user.username
-    newHash["location"] = user.location
-    newHash["email"] = user.email
-    return newHash
-}
-
-function createuserCard(hash) {
-    let h2 = document.querySelector('#userID')
-    let div = document.createElement('div')
-    div.classList.add("userInfo")
-    h2.appendChild(div)
-    let username = document.createElement('p')
-    username.textContent = hash.username
-    div.appendChild(username)
-    let email = document.createElement('p')
-    email.textContent = hash.email
-    div.appendChild(email)
     let location = document.createElement('p')
     location.textContent = hash.location
     div.appendChild(location)
